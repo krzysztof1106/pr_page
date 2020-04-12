933e7a19a75210f007936aa8887a48809e3a80e8
Krzysztof Nowak <nowak.krzysztof.bartosz@gmail.com>
2020-04-12 05:06:38 +0200

    Updates

---
+++ precache-manifest.39c64c8a15a7f8b8b1c73624e414665a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30505fbe27ba43c7154d547a80a7c5ee",
+    "url": "/pr_page_v1/index.html"
+  },
+  {
+    "revision": "e589bfa56ecb8d95796c",
+    "url": "/pr_page_v1/static/css/main.62a26665.chunk.css"
+  },
+  {
+    "revision": "f2bb07ed1256ebde5f7f",
+    "url": "/pr_page_v1/static/js/2.67115df8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/pr_page_v1/static/js/2.67115df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e589bfa56ecb8d95796c",
+    "url": "/pr_page_v1/static/js/main.538b6f60.chunk.js"
+  },
+  {
+    "revision": "55a03c118f33a6943d11",
+    "url": "/pr_page_v1/static/js/runtime-main.2ff165f1.js"
+  },
+  {
+    "revision": "4f286d942ed95f83f3f3ff38a6f0b086",
+    "url": "/pr_page_v1/static/media/FP_TeamPic.4f286d94.jpg"
+  },
+  {
+    "revision": "3b17fad6b27429ca4e2e1bc81ac63c53",
+    "url": "/pr_page_v1/static/media/Front_Pic2.3b17fad6.jpg"
+  },
+  {
+    "revision": "0f6fb33e8a9cae3572e38cf31f1fd082",
+    "url": "/pr_page_v1/static/media/MAPA.0f6fb33e.jpg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/pr_page_v1/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/pr_page_v1/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/pr_page_v1/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/pr_page_v1/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/pr_page_v1/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/pr_page_v1/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/pr_page_v1/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/pr_page_v1/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/pr_page_v1/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/pr_page_v1/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/pr_page_v1/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/pr_page_v1/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/pr_page_v1/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/pr_page_v1/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/pr_page_v1/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "a748dd73e3c31530ea4e5db0796a7e36",
+    "url": "/pr_page_v1/static/media/styliacjaWlosow.a748dd73.jpg"
+  },
+  {
+    "revision": "4f286d942ed95f83f3f3ff38a6f0b086",
+    "url": "/pr_page_v1/static/media/stylizacjaBrwi.4f286d94.jpg"
+  },
+  {
+    "revision": "c40666394d98ace6b62713d0a67cd410",
+    "url": "/pr_page_v1/static/media/stylizacjaPaznokci.c4066639.jpg"
+  },
+  {
+    "revision": "966d334df1a197ef9f617955ec2dadbe",
+    "url": "/pr_page_v1/static/media/stylizacjaRzes.966d334d.jpg"
+  }
+]);
