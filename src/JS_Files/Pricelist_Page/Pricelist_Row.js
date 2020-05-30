fbff5cbee6f02f3be8a5362254ebc26510690183
Krzysztof Nowak <nowak.krzysztof.bartosz@gmail.com>
2020-05-30 22:54:03 +0200

    Status 2020-05-30_v1

--- src/JS_Files/Pricelist_Page/Pricelist_Row.js
+++ src/JS_Files/Pricelist_Page/Pricelist_Row.js
@@ -9,29 +9,25 @@ import React from 'react';
 const PricelistRow = (props)  => {
     return (
         <div className="price_contentBox">
-            <div className="price_row">
+            <div className="price_headerBox">
                 <div className="price_headerBar"></div>
-                <div className="price_headerBox HeaderStyle">{props.header}</div>
+                <div className="price_header HeaderStyle">{props.header}</div>
                 <div className="price_headerBar"></div>
             </div>
-            <div className="price_row">
+            <div className="price_tagRow">
                 <div className="price_tagContent">keratynowe prostowanie włosów</div>
-                <div className="price_tagBar"></div>
                 <div className="price_tagContent">100zł</div>
             </div>
-            <div className="price_row">
+            <div className="price_tagRow">
                 <div className="price_tagContent">1:1</div>
-                <div className="price_tagBar"></div>
                 <div className="price_tagContent">100zł</div>
             </div>
-            <div className="price_row">
+            <div className="price_tagRow">
+                <div className="price_tagContent">2:1</div>
+                <div className="price_tagContent">100zł</div>
+            </div>
+            <div className="price_tagRow">
                 <div className="price_tagContent">3:1</div>
-                <div className="price_tagBar"></div>
-                <div className="price_tagContent">100zł</div>
-            </div>
-            <div className="price_row">
-                <div className="price_tagContent">4:1</div>
-                <div className="price_tagBar"></div>
                 <div className="price_tagContent">100zł</div>
             </div>
         </div>
